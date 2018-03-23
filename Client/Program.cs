37e9948c44d2a38badccf663d0f7e3f500a57ab8
TuanNguyen <TuanNguyen@HOANGTUAN-PC>
2018-03-23 20:33:44 +0700

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ips = new IPEndPoint(IPAddress.Loopback, 1234);
+            UdpClient client = new UdpClient();
+            client.Connect(ips);
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Nhap x: ");
+                    int x = int.Parse(Console.ReadLine());
+                    Console.Write("Nhap y: ");
+                    int y = int.Parse(Console.ReadLine());
+                    byte[] a = BitConverter.GetBytes(x);
+                    byte[] b = BitConverter.GetBytes(y);
+                    client.Send(a, a.Length);
+                    client.Send(b, b.Length);
+
+                    Console.Write("Tong x + y: ");
+                    byte[] result = client.Receive(ref ips);
+                    int tong = BitConverter.ToInt32(result, 0);
+                    Console.WriteLine(tong);
+                    Console.WriteLine("Bam 0: Tiep tuc nhap ---- Bam 1: Thoat");
+                    int i = int.Parse(Console.ReadLine());
+                    if (i == 0)
+                        continue;
+                    else
+                        break;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    break;
+                }
+            }
+            client.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            IPEndPoint ips = new IPEndPoint(IPAddress.Any, 1234);
+            UdpClient server = new UdpClient(ips);
+            IPEndPoint ipc = new IPEndPoint(IPAddress.Any, 0);
+            while (true)
+            {
+                try
+                {
+                    byte[] a = server.Receive(ref ipc);
+                    int x = BitConverter.ToInt32(a, 0);
+                    byte[] b = server.Receive(ref ipc);
+                    int y = BitConverter.ToInt32(b, 0);
+
+                    int tong = x + y;
+
+                    byte[] result = BitConverter.GetBytes(tong);
+                    server.Send(result, result.Length, ipc);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    break;
+                }
+            }
+            server.Close();
+        }
+    }
+}
